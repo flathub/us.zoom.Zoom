6f7306569dd9ca2f4acccf6df98d6cce5117acbd
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-11-01 13:31:03 -0500

    Simplify script even more

--- zoom.sh
+++ zoom.sh
@@ -6,12 +6,11 @@ if [[ -f "$ZOOMUS" ]]; then
     :
 else
     ln -s $HOME/.var/app/us.zoom.Zoom/config/* ~/.var/app/us.zoom.Zoom/.config
-fi
-
-if [[ -f "$ZOOMUS" ]]; then
-    sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
-else
-    echo "zoomus.conf does not exist yet. Please re-run Zoom for it to automatically enable Wayland."
+    if [[ -f "$ZOOMUS" ]]; then
+    	sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
+    else
+    	echo "zoomus.conf does not exist yet. Please re-run Zoom for it to automatically enable Wayland."
+    fi
 fi
 
 exec env TMPDIR=$XDG_CACHE_HOME /app/extra/zoom/ZoomLauncher "$@"
