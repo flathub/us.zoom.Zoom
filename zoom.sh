ad0f11020b4d68048c6c8e74965e30a9f84ce0b8
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-11-01 13:17:20 -0500

    Simplify script

--- zoom.sh
+++ zoom.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 ZOOMUS=$HOME/.var/app/us.zoom.Zoom/.config/zoomus.conf
+
 if [[ -f "$ZOOMUS" ]]; then
     :
 else
     ln -s $HOME/.var/app/us.zoom.Zoom/config/* ~/.var/app/us.zoom.Zoom/.config
 fi
 
-ZOOMUS_WAYLAND=$HOME/.var/app/us.zoom.Zoom/.config/zoomus.conf
-if [[ -f "$ZOOMUS_WAYLAND" ]]; then
+if [[ -f "$ZOOMUS" ]]; then
     sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
 else
-    echo "File does not exist yet. Re-run Zoom to successfully symlink."
+    echo "zoomus.conf does not exist yet. Please re-run Zoom for it to automatically enable Wayland."
 fi
 
 exec env TMPDIR=$XDG_CACHE_HOME /app/extra/zoom/ZoomLauncher "$@"
