4218cd63ce6bbfd2c3e841c5c1b46255ab3cbebb
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-11-05 13:40:26 -0500

    parent 2762935ae4339d4c60d41f32a4b9cef9741272e6 author TheEvilSkeleton <theevilskeleton@riseup.net> 1604601626 -0500 committer TheEvilSkeleton <theevilskeleton@riseup.net> 1604608214 -0500
    parent 2762935ae4339d4c60d41f32a4b9cef9741272e6
    author TheEvilSkeleton <theevilskeleton@riseup.net> 1604601626 -0500
    committer TheEvilSkeleton <theevilskeleton@riseup.net> 1604608212 -0500
    
    parent 2762935ae4339d4c60d41f32a4b9cef9741272e6
    author TheEvilSkeleton <theevilskeleton@riseup.net> 1604601626 -0500
    committer TheEvilSkeleton <theevilskeleton@riseup.net> 1604608206 -0500
    
    parent 2762935ae4339d4c60d41f32a4b9cef9741272e6
    author TheEvilSkeleton <theevilskeleton@riseup.net> 1604601626 -0500
    committer TheEvilSkeleton <theevilskeleton@riseup.net> 1604608201 -0500
    
    parent 2762935ae4339d4c60d41f32a4b9cef9741272e6
    author TheEvilSkeleton <theevilskeleton@riseup.net> 1604601626 -0500
    committer TheEvilSkeleton <theevilskeleton@riseup.net> 1604608184 -0500
    
    Enable Wayland support

--- zoom.sh
+++ zoom.sh
@@ -8,15 +8,10 @@ if [[ -f "$ZOOMUS" ]]; then
     :
 else
     # If ~/.var/app/us.zoom.Zoom/.config/zoomus.conf doesn't exist, symlink ~/.var/app/us.zoom.Zoom/config/ to ~/.var/app/us.zoom.Zoom/.config.
-    ln -s $HOME/.var/app/us.zoom.Zoom/config/* ~/.var/app/us.zoom.Zoom/.config
-    # Check (again) if ~/.var/app/us.zoom.Zoom/.config/zoomus.conf exists.
-    if [[ -f "$ZOOMUS" ]]; then
-        # If it does, replace enableWaylandShare=false to enableWaylandShare=true in ~/.var/app/us.zoom.Zoom/config/zoomus.conf.
-    	sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
-    else
-        # If it doesn't, echo:
-    	echo "zoomus.conf does not exist yet. Please re-run Zoom for it to automatically enable Wayland."
-    fi
+    ln -s $HOME/.var/app/us.zoom.Zoom/config/ ~/.var/app/us.zoom.Zoom/.config
 fi
+
+# Enable Wayland
+sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
 
 exec env TMPDIR=$XDG_CACHE_HOME /app/extra/zoom/ZoomLauncher "$@"
