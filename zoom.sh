bf9f515cabcbd98d0f8bd123f09e09d42477d28c
Kai Mast <kaimast@cs.wisc.edu>
2021-06-01 14:29:45 -0500

    Use QPA=wayland instead of QPA=wayland-egl

--- zoom.sh
+++ zoom.sh
@@ -4,20 +4,20 @@ CONF_FILE=$HOME/.zoom/config/zoomus.conf
 
 # Check if the current desktop is using Wayland
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-    QPA=wayland-egl
+    QPA=wayland
 
-	# Check if the line 'enableWaylandShare' is set to 'true' in $HOME/.var/app/us.zoom.Zoom/config/zoomus.conf
-	if ! grep -q enableWaylandShare=true "$CONF_FILE"; then
+    # Check if the line 'enableWaylandShare' is set to 'true' in $HOME/.var/app/us.zoom.Zoom/config/zoomus.conf
+    if ! grep -q enableWaylandShare=true "$CONF_FILE"; then
 
-		# Replace enableWaylandShare=false to enableWaylandShare=true
-		sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' $CONF_FILE
+        # Replace enableWaylandShare=false to enableWaylandShare=true
+        sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' $CONF_FILE
 
-		# Recheck if the line 'enableWaylandShare' is set to true in $HOME/.var/app/us.zoom.Zoom/config/zoomus.conf
-		if ! grep -q enableWaylandShare=true "$CONF_FILE"; then
+        # Recheck if the line 'enableWaylandShare' is set to true in $HOME/.var/app/us.zoom.Zoom/config/zoomus.conf
+        if ! grep -q enableWaylandShare=true "$CONF_FILE"; then
 
-			# If not, create a GTK dialog that says the string inside '--text'
+            # If not, create a GTK dialog that says the string inside '--text'
             zenity --error --text="Wayland screen sharing is not yet enabled. Please restart Zoom for it to automatically enable, or manually change the value of \"enableWaylandShare\" to \"true\" in \"$CONF_FILE\"."
-		fi
+        fi
     fi
 else 
     QPA=xcb
