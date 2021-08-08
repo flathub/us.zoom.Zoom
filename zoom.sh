ae067de57017a636114b80e9066f66fdee5864bc
TheEvilSkeleton <theevilskeleton@riseup.net>
2021-08-08 12:29:33 -0400

    Improve shell script
    - Use double quotes to prevent globbing
    - Use tab on a line to improve indentation

--- zoom.sh
+++ zoom.sh
@@ -7,7 +7,7 @@ if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
 	if ! grep -q enableWaylandShare=true "$HOME/.var/app/$FLATPAK_ID/config/zoomus.conf"; then
 
 		# Replace enableWaylandShare=false to enableWaylandShare=true
-		sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/$FLATPAK_ID/config/zoomus.conf
+		sed -i 's/enableWaylandShare\=false/enableWaylandShare\=true/' ~/.var/app/"$FLATPAK_ID"/config/zoomus.conf
 
 		# Recheck if the line 'enableWaylandShare' is set to true in $HOME/.var/app/us.zoom.Zoom/config/zoomus.conf
 		if ! grep -q enableWaylandShare=true "$HOME/.var/app/$FLATPAK_ID/config/zoomus.conf"; then
@@ -16,7 +16,7 @@ if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
         	       	zenity --error --text="Wayland screen sharing is not yet enabled. Please restart Zoom for it to automatically enable, or manually change the value of \"enableWaylandShare\" to \"true\" in \"$HOME/.var/app/$FLATPAK_ID/config/zoomus.conf\"."
 
 		fi
-       	fi
+	fi
 fi
 
-exec env TMPDIR=$XDG_CACHE_HOME /app/extra/zoom/ZoomLauncher "$@"
+exec env TMPDIR="$XDG_CACHE_HOME" /app/extra/zoom/ZoomLauncher "$@"
